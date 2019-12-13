1694234aa89b44375f57fa54c9fd666eaa41b570
Joanne Stasiak <joanne.stasiak@temple.edu>
2019-12-13 15:09:13 -0500

    hi

--- FoodRegulation/FoodRegPavlovia_lastrun.py
+++ FoodRegulation/FoodRegPavlovia_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.2.4),
-    on December 12, 2019, at 11:23
+    on December 13, 2019, at 10:12
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -208,6 +208,16 @@ text_2 = visual.TextStim(win=win, name='text_2',
     text='default text',
     font='Arial',
     pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
+    color='white', colorSpace='rgb', opacity=1, 
+    languageStyle='LTR',
+    depth=0.0);
+
+# Initialize components for Routine "ThankYou"
+ThankYouClock = core.Clock()
+ty = visual.TextStim(win=win, name='ty',
+    text='Thank you for participating!\n\nYou have completed this part of the study',
+    font='Arial',
+    pos=(0, 0), height=0.08, wrapWidth=1.4, ori=0, 
     color='white', colorSpace='rgb', opacity=1, 
     languageStyle='LTR',
     depth=0.0);
@@ -796,9 +806,9 @@ thisExp.nextEntry()
 routineTimer.reset()
 
 # set up handler to look after randomisation of conditions etc
-trials_2 = data.TrialHandler(nReps=1, method='random', 
+trials_2 = data.TrialHandler(nReps=1, method='sequential', 
     extraInfo=expInfo, originPath=-1,
-    trialList=data.importConditions('foodRows.xlsx'),
+    trialList=data.importConditions('foodRows.csv'),
     seed=None, name='trials_2')
 thisExp.addLoop(trials_2)  # add the loop to the experiment
 thisTrial_2 = trials_2.trialList[0]  # so we can initialise stimuli with some values
@@ -905,7 +915,7 @@ for thisTrial_2 in trials_2:
     trials_2.addData('ISI.stopped', ISI.tStopRefresh)
     
     # set up handler to look after randomisation of conditions etc
-    mainTrials = data.TrialHandler(nReps=1, method='random', 
+    mainTrials = data.TrialHandler(nReps=1, method='sequential', 
         extraInfo=expInfo, originPath=-1,
         trialList=data.importConditions('FoodReg.csv', selection=rows),
         seed=None, name='mainTrials')
@@ -1175,6 +1185,68 @@ for thisTrial_2 in trials_2:
     
 # completed 1 repeats of 'trials_2'
 
+
+# ------Prepare to start Routine "ThankYou"-------
+# update component parameters for each repeat
+# keep track of which components have finished
+ThankYouComponents = [ty]
+for thisComponent in ThankYouComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+ThankYouClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+frameN = -1
+continueRoutine = True
+
+# -------Run Routine "ThankYou"-------
+while continueRoutine:
+    # get current time
+    t = ThankYouClock.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=ThankYouClock)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *ty* updates
+    if ty.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        ty.frameNStart = frameN  # exact frame index
+        ty.tStart = t  # local t and not account for scr refresh
+        ty.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(ty, 'tStartRefresh')  # time at next scr refresh
+        ty.setAutoDraw(True)
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in ThankYouComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "ThankYou"-------
+for thisComponent in ThankYouComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+thisExp.addData('ty.started', ty.tStartRefresh)
+thisExp.addData('ty.stopped', ty.tStopRefresh)
+# the Routine "ThankYou" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
 
 # Flip one final time so any remaining win.callOnFlip() 
 # and win.timeOnFlip() tasks get executed before quitting
