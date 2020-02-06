fcc1b90c612a87ed62e95ec9014c1cada61800ef
Joanne Stasiak <joanne.stasiak@temple.edu>
2020-02-06 10:06:50 -0500

    commit

--- Moral Self-Regulation/MoralSRBinaryChoice_lastrun.py
+++ Moral Self-Regulation/MoralSRBinaryChoice_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.2.4),
-    on December 18, 2019, at 12:43
+    on February 06, 2020, at 09:42
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
